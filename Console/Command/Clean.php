a44990ba5f999eb133b2d37dcb0021ee71f553e7
Dominic <dominic.watts@hotmail.co.uk>
2020-02-22 23:43:51 +0000

    1.0.4, house keeping

--- Console/Command/Clean.php
+++ Console/Command/Clean.php
@@ -1,13 +1,11 @@
 <?php
 
-
 namespace Xigen\QuoteCleaner\Console\Command;
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
--- Cron/Cleaner.php
+++ Cron/Cleaner.php
@@ -1,6 +1,5 @@
 <?php
 
-
 namespace Xigen\QuoteCleaner\Cron;
 
 /**
